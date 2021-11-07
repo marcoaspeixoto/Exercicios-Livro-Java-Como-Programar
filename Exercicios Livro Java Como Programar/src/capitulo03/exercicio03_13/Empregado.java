c5889242c090af5a7a09a872d27bc6a238444153
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-11-06 22:43:20 -0300

    exercicio 03_13

---
+++ Exercicios Livro Java Como Programar/src/capitulo03/exercicio03_13/Empregado.java
@@ -0,0 +1,46 @@
+package capitulo03.exercicio03_13;
+
+public class Empregado {
+    
+    String nome;
+    String sobrenome;
+    double salario;
+
+    public Empregado(String nome, String sobrenome, double salario) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        
+        if (salario > 0) {
+            this.salario = salario;
+        }
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public void setSalario(double salario) {
+        
+        if (salario < 0) {
+            return;
+        }
+        
+        this.salario = salario;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+}
---
+++ Exercicios Livro Java Como Programar/src/capitulo03/exercicio03_13/EmpregadoTeste.java
@@ -0,0 +1,38 @@
+package capitulo03.exercicio03_13;
+
+public class EmpregadoTeste {
+    
+    public static void main(String[] args) {
+        
+        Empregado empregado1 = new Empregado("Marco", "Peixoto", 3000);
+        Empregado empregado2 = new Empregado("Geiza", "Silva", 2000);
+        
+        System.out.printf("Nome............: %s%n", empregado1.getNome());
+        System.out.printf("Sobrenome.......: %s%n", empregado1.getSobrenome());
+        System.out.printf("Salário Mensal..: R$ %,.2f%n", empregado1.getSalario());
+        System.out.printf("Salário Anual..: R$ %,.2f%n%n", empregado1.getSalario() * 12);
+    
+        System.out.printf("Nome............: %s%n", empregado2.getNome());
+        System.out.printf("Sobrenome.......: %s%n", empregado2.getSobrenome());
+        System.out.printf("Salário Mensal..: R$ %,.2f%n", empregado2.getSalario());
+        System.out.printf("Salário Anual...: R$ %,.2f%n", empregado2.getSalario() * 12);
+        
+        System.out.print("\nAplicando aumento de 10% no salário\n\n");
+        
+        double novoSalario = empregado1.getSalario() * (1 + 10.0 / 100);
+        empregado1.setSalario(novoSalario);
+        
+        novoSalario = empregado2.getSalario() * (1 + 10.0 / 100);
+        empregado2.setSalario(novoSalario);
+        
+        System.out.printf("Nome............: %s%n", empregado1.getNome());
+        System.out.printf("Sobrenome.......: %s%n", empregado1.getSobrenome());
+        System.out.printf("Salário Mensal..: R$ %,.2f%n", empregado1.getSalario());
+        System.out.printf("Salário Anual..: R$ %,.2f%n%n", empregado1.getSalario() * 12);
+    
+        System.out.printf("Nome............: %s%n", empregado2.getNome());
+        System.out.printf("Sobrenome.......: %s%n", empregado2.getSobrenome());
+        System.out.printf("Salário Mensal..: R$ %,.2f%n", empregado2.getSalario());
+        System.out.printf("Salário Anual...: R$ %,.2f%n", empregado2.getSalario() * 12);
+    }
+}
