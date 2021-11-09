e1ad059697a1458509690942bea1d09f251e71ca
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-11-09 17:09:30 -0300

    exercicio 03_14

---
+++ Exercicios Livro Java Como Programar/src/exercicio03_14/Data.java
@@ -0,0 +1,41 @@
+package exercicio03_14;
+
+public class Data {
+    
+    int mes, dia, ano;
+    
+    Data(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+    
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+    
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+    
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+    
+    public int getDia() {
+        return dia;
+    }
+    
+    public int getMes() {
+        return mes;
+    }
+    
+    public int getAno() {
+        return ano;
+    }
+    
+    public String exibeData() {
+        String dataFormatada = String.format("%02d/%02d/%d%n", getDia(), getMes(), getAno());
+        return dataFormatada;
+    }
+}
---
+++ Exercicios Livro Java Como Programar/src/exercicio03_14/DataTeste.java
@@ -0,0 +1,29 @@
+package exercicio03_14;
+
+import java.util.Scanner;
+
+public class DataTeste {
+    
+    public static void main(String[] args) {
+        
+        Data dataHoje = new Data(9, 11, 2021);
+        
+        System.out.printf("Data de hoje: %s", dataHoje.exibeData());
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.print("Altere o dia: ");
+        int dia = entrada.nextInt();
+        dataHoje.setDia(dia);
+        
+        System.out.print("Altere o mês: ");
+        int mes = entrada.nextInt();
+        dataHoje.setMes(mes);
+        
+        System.out.print("Altere o ano: ");
+        int ano = entrada.nextInt();
+        dataHoje.setAno(ano);
+        
+        System.out.printf("Data de hoje alterada: %s", dataHoje.exibeData());
+    }
+}
